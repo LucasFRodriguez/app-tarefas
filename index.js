8e10f05cdf5eb84e250685a8e3fadef979e1743b
LucasFRodriguez <lucasfrz26@gmail.com>
2024-09-11 12:29:41 -0300

    inciando menu

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-function start(){
-    console.log('Funcionando');
+// Biblioteca INQUIRER
+const { select } = require('@inquirer/prompts');
+
+const start = async () => {
+    
+    while(true){
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    nome: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        });
+
+        switch(opcao){
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break;
+            case "Listar":
+                console.log("vamos Listar")
+                break;
+            case "sair":
+                console.log("Até a próxima! Tenha um otimo dia e realize todas as suas metas!")
+                return
+            }
+
+    }
+    
 }
 
 start();
