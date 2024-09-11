3df0128010764cf904b9545c02d90c7cc8564cbc
LucasFRodriguez <lucasfrz26@gmail.com>
2024-09-11 13:19:46 -0300

    listarMetas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Biblioteca INQUIRER
-const { select, input } = require('@inquirer/prompts');
+const { select, input, checkbox } = require('@inquirer/prompts');
 
 let meta = {
     value: 'Criar novas metas!',
@@ -11,12 +11,42 @@ let metas = [ meta ];
 const cadastrarMeta = async () => {
     const meta = await input({message: "Digite sua nova meta:"})
 
+    // apenas entra nesse if caso o usuario nao escrever nada na hora de criar uma nova meta!
     if(meta.length == 0){
         console.log('A meta não pode ser vazia!')
         return
     }
     metas.push({ value: meta, checked: false })
 }
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as Setas para mudar de meta, o Espaço para marcar ou desmarcar e o Enter para finalizar essa etapa.",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length == 0){
+        console.log("Nenhuma meta selecionada.")
+        return
+    }
+
+    metas.forEach((m) =>{
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true 
+
+    })
+
+    console.log('Meta(s) marcadas como concluidas')
+
+};
 
 const start = async () => {
     
@@ -30,7 +60,7 @@ const start = async () => {
                     value: "cadastrar"
                 },
                 {
-                    nome: "Listar metas",
+                    name: "Listar metas",
                     value: "listar"
                 },
                 {
@@ -42,11 +72,11 @@ const start = async () => {
 
         switch(opcao){
             case "cadastrar":
-                await cadastrarMeta();
+                await cadastrarMetas();
                 console.log(metas);
                 break;
-            case "Listar":
-                console.log("vamos Listar")
+            case "listar":
+                await listarMetas();
                 break;
             case "sair":
                 console.log("Até a próxima! Tenha um otimo dia e realize todas as suas metas!")
