016e34435a455ee9dc70e6c6dbe77acba3ff1336
LucasFRodriguez <lucasfrz26@gmail.com>
2024-09-11 12:46:44 -0300

    cadastrarMeta

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // Biblioteca INQUIRER
-const { select } = require('@inquirer/prompts');
+const { select, input } = require('@inquirer/prompts');
+
+let meta = {
+    value: 'Criar novas metas!',
+    checked: false,
+};
+
+let metas = [ meta ];
+
+const cadastrarMeta = async () => {
+    const meta = await input({message: "Digite sua nova meta:"})
+
+    if(meta.length == 0){
+        console.log('A meta não pode ser vazia!')
+        return
+    }
+    metas.push({ value: meta, checked: false })
+}
 
 const start = async () => {
     
@@ -25,7 +42,8 @@ const start = async () => {
 
         switch(opcao){
             case "cadastrar":
-                console.log("vamos cadastrar")
+                await cadastrarMeta();
+                console.log(metas);
                 break;
             case "Listar":
                 console.log("vamos Listar")
