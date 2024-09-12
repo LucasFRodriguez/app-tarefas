0b97f15c06597a38b44a37d5e8968242d1b03d58
LucasFRodriguez <lucasfrz26@gmail.com>
2024-09-12 13:25:01 -0300

    metasRealizadas

--- index.js
+++ index.js
@@ -48,6 +48,23 @@ const listarMetas = async () => {
 
 };
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter(( meta ) => { 
+        return meta.checked
+     })
+
+     if(realizadas.length == 0){
+        console.log("Não existem metas realizadas! :(")
+        return
+     }
+
+     await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+     })
+
+}
+
 const start = async () => {
     
     while(true){
@@ -62,6 +79,10 @@ const start = async () => {
                 {
                     name: "Listar metas",
                     value: "listar"
+                },                
+                {
+                    name: "Metas Realizadas",
+                    value: "realizadas"
                 },
                 {
                     name: "Sair",
@@ -77,6 +98,9 @@ const start = async () => {
                 break;
             case "listar":
                 await listarMetas();
+                break;
+            case "realizadas":
+                await metasRealizadas();
                 break;
             case "sair":
                 console.log("Até a próxima! Tenha um otimo dia e realize todas as suas metas!")
